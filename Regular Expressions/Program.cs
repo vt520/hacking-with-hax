bed2e8e9338d5a90a4d4acd5ddd34d8098695d2a
vt520 <mark@energyservices.org>
2022-07-02 21:59:24 -0700

    Here's something to start with

--- Regular Expressions/Program.cs
+++ Regular Expressions/Program.cs
@@ -1,7 +1,17 @@
-﻿namespace Regular_Expressions {
+﻿using System.Text.RegularExpressions;
+
+namespace Regular_Expressions {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            List<string> word_list = new() {
+                "get", "set", "while"
+            };
+            Regex tag_matcher = new(@"[<]([^<>]+)[>]"); // Use _Verbatim Strings_ for Regular expressions
+            List<Regex> expressions = new() { tag_matcher };
+            foreach (string word in word_list) {
+                expressions.Add(new($@"\b({word})\b"));
+            }
+            
         }
     }
 }
