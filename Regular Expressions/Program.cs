983d721519682c62778c5b2eb09b5326ef03c780
vt520 <mark@energyservices.org>
2022-07-02 22:17:04 -0700

    Here

--- Regular Expressions/Program.cs
+++ Regular Expressions/Program.cs
@@ -2,16 +2,38 @@
 
 namespace Regular_Expressions {
     internal class Program {
+        const string TestText = 
+@"this is come <test> text
+I also included a set of keywords while making sure
+that words only whole words get selected";
+
+        public enum Colours {
+            Black,
+            Green,
+            Navy_Pink,
+            Courdoroy
+        }
         static void Main(string[] args) {
+
             List<string> word_list = new() {
-                "get", "set", "while"
+                "get", "set", "while", "select"
             };
+
+            Dictionary<Regex, Colours> color_map = new() {
+                // for later
+            };
+
             Regex tag_matcher = new(@"[<]([^<>]+)[>]"); // Use _Verbatim Strings_ for Regular expressions
             List<Regex> expressions = new() { tag_matcher };
             foreach (string word in word_list) {
                 expressions.Add(new($@"\b({word})\b"));
             }
-            
+            foreach (Regex expression in expressions) {
+                MatchCollection matched = expression.Matches(TestText);
+                foreach (Match match in matched) {
+                    // try watching what happens here
+                }
+            }
         }
     }
 }
