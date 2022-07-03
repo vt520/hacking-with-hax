7be546f40756deb7fdf4f6037801934b527d990d
vt520 <mark@energyservices.org>
2022-07-02 22:49:15 -0700

    Bade Idea

---
+++ Regular Expressions/WrongWayToMatchStrings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regular_Expressions {
+    internal class WrongWayToMatchStrings {
+        public (long open, long close) GetTag(string text, int start = 0) {
+            int first_open = text.IndexOf('<', start);
+            int second_open = text.IndexOf('<', first_open);
+            int close = text.IndexOf('>', start);
+            if (second_open > close) return GetTag(text, second_open);
+            return (first_open, close);
+        }
+    }
+}
