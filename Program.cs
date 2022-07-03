76f917e8175feb11d96403e70995c071b467b71f
vt520 <mark@energyservices.org>
2022-07-02 21:51:14 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿namespace Hacking_with_Hax {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Regular Expressions/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace Regular_Expressions {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
